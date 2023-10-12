448d350aa803e27c85be0aded78788a315122dac
Antonio Espinosa <antespi@gmail.com>
2023-10-12 14:47:37 +0200

    Improve image scripts

--- convert_img.sh
+++ convert_img.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-IMG_PATH="assets/images"
+IMG_PATH=${1:-assets/images}
+IMG_PATH=${IMG_PATH%/}
 cd "$SCRIPT_DIR"
 
+echo "Processing folder: $IMG_PATH"
 for image in $IMG_PATH/*.jpg; do
   base_img=`basename "${image%.jpg}"`
   webp_img="${base_img}.webp"
--- resize_img.sh
+++ resize_img.sh
@@ -3,25 +3,33 @@
 # Example: ./resize_img.sh filename.jpg 1280 640 480 320
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-IMG_PATH="assets/images"
 cd "$SCRIPT_DIR"
 
 filename=$1
+base_name=$(basename -- "$filename")
+extension="${base_name##*.}"
+image="${base_name%.*}"
+path="${filename%$base_name}"
+path="${path%%/}"
+if [ -z $path ]; then path="."; fi
+
+# echo "Filename: $filename"
+# echo "Basename: $base_name"
+# echo "Extension: $extension"
+# echo "Image: $image"
+# echo "Path: $path"
 shift
+
 
 if [ -z "$filename" ]; then
   echo "ERROR: You need to set the image filename to resize"
   exit 1
 fi
 
-if [ ! -f $IMG_PATH/$filename ]; then
-  echo "ERROR: Image '$IMG_PATH/$filename' not found"
+if [ ! -f $filename ]; then
+  echo "ERROR: Image '$filename' not found"
   exit 2
 fi
-
-base_name=$(basename -- "$filename")
-extension="${base_name##*.}"
-image="${base_name%.*}"
 
 if [ "$extension" != "jpg" ]; then
   echo "ERROR: Image extension is not .jpg"
@@ -31,8 +39,13 @@ fi
 while (( "$#" )); do
 
 width=$1
-echo "convert -resize ${width}x $IMG_PATH/$filename -quality 90 $IMG_PATH/${image}_${width}.jpg"
-convert -resize ${width}x $IMG_PATH/$filename -quality 90 $IMG_PATH/${image}_${width}.jpg
+echo -n "$path/${image}_${width}.jpg ... "
+if [ ! -f "$path/${image}_${width}.jpg" ]; then
+  echo "convert -resize ${width}x $filename -quality 90"
+  convert -resize ${width}x "$filename" -quality 90 "$path/${image}_${width}.jpg"
+else
+   echo "OK"
+fi
 
 shift
 
